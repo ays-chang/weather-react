eace230988135991af2f784f28d263e1a35bffd4
pikotato <73317625+pikotato@users.noreply.github.com>
2021-04-29 21:41:28 -0700

    Use API icon url

--- src/Search.js
+++ src/Search.js
@@ -15,7 +15,7 @@ export default function Search(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconURL: "http://openweathermap.org/img/wn/50d@2x.png",
+      iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
